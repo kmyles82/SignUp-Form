8a3fa37dab16ee3256dae425b08f33647c286807
kmyles82 <kmyles82@gmail.com>
2019-07-28 12:31:09 -0500

    add getters

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -2,38 +2,23 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
-    <p>Your email address: {{ email }}</p>
+    <p v-if="email">Your email address: {{ email }}</p>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+  import axios from 'axios';
 
-export default {
-  data(){
-    return{
-      email: ''
-    }
-  },
-  created(){
-    axios.get('/users.json')
-    .then(res => {
-      console.log(res)
-      const data = res.data;
-      const users = []
-      for(let key in data){
-        const user = data[key]
-        user.id = key;
-        users.push(user)
+  export default {
+    computed: {
+      email () {
+        return !this.$store.getters.user ? false : this.$store.getters.user.email
       }
-      console.log(users)
-      this.email = users[0].email
-    })
-    .catch(error => {
-      console.log(error)
-    })
-  },
-}
+    },
+    created () {
+      this.$store.dispatch('fetchUser')
+    }
+  }
 </script>
 
 <style scoped>
