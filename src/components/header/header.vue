00d26bff04149c981456b2038179c7cec6e175af
kmyles82 <kmyles82@gmail.com>
2019-07-28 12:30:28 -0500

    added logout button and authentication

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -5,19 +5,38 @@
     </div>
     <nav>
       <ul>
-        <li>
+        <li v-if="!auth">
           <router-link to="/signup">Sign Up</router-link>
         </li>
-        <li>
+        <li v-if="!auth">
           <router-link to="/signin">Sign In</router-link>
         </li>
-        <li>
+        <li v-if="auth">
           <router-link to="/dashboard">Dashboard</router-link>
+        </li>
+        <li v-if="auth">
+          <button class="logout" @click="onLogout">Logout</button>
         </li>
       </ul>
     </nav>
   </header>
 </template>
+
+<script>
+
+export default {
+  computed:{
+    auth(){
+      return this.$store.getters.isAuthenticated
+    }
+  },
+  methods: {
+    onLogout(){
+      this.$store.dispatch('logout')
+    }
+  },
+}
+</script>
 
 <style scoped>
   #header {
@@ -67,5 +86,13 @@
   li a:active,
   li a.router-link-active {
     color: #fa923f;
+  }
+
+  .logout{
+    background: transparent;
+    border: none;
+    font: inherit;
+    cursor: pointer;
+    color: #fff;
   }
 </style>
